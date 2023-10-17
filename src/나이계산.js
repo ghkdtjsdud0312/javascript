10b4dbfe1aaf5a614564bcee05d913aa1c63810c
ghkdtjsdud0312 <ghkdtjsdud0312@gmail.com>
2023-10-17 12:01:53 +0900

    10/17

--- src/나이계산.js
+++ src/나이계산.js
@@ -3,6 +3,6 @@ let birthYear;
 let age;
 
 birthYear = prompt("태어난 연도를 입력하세요(YYYY) : ", "");
-age = currentYear - birthYear;
+age = currentYear - birthYea + 1; // Number(birthYear);
 document.write(currentYear + "년 현재<br>") // 웹화면에 보여지는 부분
 document.write(birthYear + "년에 태어난 사람의 나이는 " + age + "살 입니다.");
