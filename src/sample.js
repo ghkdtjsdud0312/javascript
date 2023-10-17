e45cf79fb25db27efcfccd08b3f222b646fd87ae
ghkdtjsdud0312 <ghkdtjsdud0312@gmail.com>
2023-10-17 10:53:22 +0900

    10/17

--- src/sample.js
+++ src/sample.js
@@ -3,10 +3,10 @@ const head = document.querySelector("#head-line");
 head.onclick = function() {
     if(!isRed) {
         head.style.color = "red";
-        console.log("현재 컬러는 레드 입니다.")
+        console.log("현재 컬러는 레드 입니다.");
     } else {
         head.style.color = "blue";
-        console.log("현재 컬러는 블루 입니다.")
+        console.log("현재 컬러는 블루 입니다.");
     }
     isRed = !isRed;
     }
