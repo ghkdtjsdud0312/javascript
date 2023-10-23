6c9e3dcee4a725a0ce8cfd19be95367c6aa53fb5
ghkdtjsdud0312 <ghkdtjsdud0312@gmail.com>
2023-10-23 12:13:27 +0900

    10/23

---
+++ src/RestAPI.js
@@ -0,0 +1,12 @@
+fetch("http://localhost:3000/posts")
+    .then((response) => response.json())
+    .then((json) => console.log(json)); //전체 데이터 조회
+
+fetch("http://localhost:3000/comments/1")
+    .then((response) => response.json())
+    .then((json) => console.log(json)); //id로 조회
+
+fetch("http://localhost:3000/comments?postId=1")
+    .then((response) => response.json())
+    .then((json) => console.log(json)); //query로 postId=1로 조회
+
--- src/ajax.js
+++ src/ajax.js
@@ -1,5 +1,5 @@
 const xhr = new XMLHttpRequest(); // XMLHttpRequest 객체 생성
-xhr.open("POST", "https://jsonplaceholder.typicode.com/posts"); // HTTP Method, URL 정의
+xhr.open("GET", "https://jsonplaceholder.typicode.com/posts"); // HTTP Method, URL 정의
 xhr.setRequestHeader("content-type", "application/json"); // 헤더값의 content-type 정의
 
 const data = {
@@ -8,10 +8,10 @@ const data = {
     author : "곰돌이사육사"
 };
 
-xhr.send(JSON.stringify(data)); // JSON.stringify를 통해 오브젝트를 문자열 형태로 변경해서 전송
+xhr.send(); // JSON.stringify를 통해 오브젝트를 문자열 형태로 변경해서 전송
 
 xhr.onload = () => {
-    if(xhr.status === 201) {
+    if(xhr.status === 200) {
         const res = JSON.parse(xhr.response);
         console.log(res);
     } else {
